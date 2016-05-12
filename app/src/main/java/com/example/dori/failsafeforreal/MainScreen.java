df187d4ca42ca26fafc25cf7d1c4e1f259071578
packerfan626 <dori_mouawad@hotmail.com>
2016-05-12 00:06:26 -0700

    Second Commit: Map not working

--- app/src/main/java/com/example/dori/failsafeforreal/MainScreen.java
+++ app/src/main/java/com/example/dori/failsafeforreal/MainScreen.java
@@ -1,7 +1,11 @@
 package com.example.dori.failsafeforreal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        //Settings Button Declaration
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.settingsImageButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                settingsButton(v);
+            }
+        });
+
+        final Button mapButton = (Button) findViewById(R.id.nearybyAlertsButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mapButton(v);
+            }
+        });
+
     }
+
+    protected void settingsButton(View v){
+        startActivity(new Intent(MainScreen.this, MainSettingsPage.class));
+    }
+
+    protected void mapButton(View v){
+        startActivity(new Intent(MainScreen.this, mapView.class));
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/dori/failsafeforreal/mapView.java
@@ -0,0 +1,54 @@
+package com.example.dori.failsafeforreal;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapView extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        Location location = null;
+        location = mMap.getMyLocation();
+
+
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
