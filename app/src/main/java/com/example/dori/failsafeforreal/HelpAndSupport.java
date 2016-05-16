6b3c78564fe77f908b37411e94c957fdd969b236
packerfan626 <dori_mouawad@hotmail.com>
2016-05-15 18:23:36 -0700

    Fourth Commit: Settings Implemented. Half-completed

---
+++ app/src/main/java/com/example/dori/failsafeforreal/HelpAndSupport.java
@@ -0,0 +1,13 @@
+package com.example.dori.failsafeforreal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpAndSupport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_and_support);
+    }
+}
--- app/src/main/java/com/example/dori/failsafeforreal/MainSettingsPage.java
+++ app/src/main/java/com/example/dori/failsafeforreal/MainSettingsPage.java
@@ -1,7 +1,10 @@
 package com.example.dori.failsafeforreal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainSettingsPage extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainSettingsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_settings_page);
+
+        //Settings Buttons Declaration
+        Button applicationButton = (Button) findViewById(R.id.applicationButton);
+        applicationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainSettingsPage.this, ApplicationSettings.class));
+            }
+        });
+
+        Button accountButton = (Button) findViewById(R.id.accountButton);
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button helpButton = (Button) findViewById(R.id.helpButton);
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainSettingsPage.this, HelpAndSupport.class));
+            }
+        });
+
+        Button termsButton = (Button) findViewById(R.id.termsButton);
+        termsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
